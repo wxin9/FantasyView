0330da69b8f4c05517cd0295d76485c5e6641c0f
Kaiser <wxin9@localhost.(none)>
2012-07-03 10:32:13 +0800

    Initial Commit

---
+++ FantasyView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FantasyView
+//
+//  Created by 昕 王 on 12-7-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
